8f0936a2fe4f60aa6d22a4bbacb23bf5c379ba95
cahaya selfi <see190523>
2026-09-20 10:12:18 +0700

    cahayacantik

---
+++ latihanpelcod.py
@@ -0,0 +1,17 @@
+# nama_peminjam = input()
+# no_aggota = 23
+# jumlah_buku = 5
+# status = True
+# durasi = 8
+
+# print("nama_peminjam")
+
+jumlah_tiket = "5"
+harga_tiket = 25000
+
+total_sementara=jumlah_tiket*2
+print("Total sementara:",total_sementara)
+
+jumlah_tiket = int(jumlah_tiket)
+total_akhir = jumlah_tiket * harga_tiket
+print("Total akhir:",total_akhir)
