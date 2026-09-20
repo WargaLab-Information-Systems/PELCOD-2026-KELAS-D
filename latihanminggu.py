655df08d4ffd60db7c019615ef3e7bf58c556723
unknown <erinvidya2104@gmail.com>
2026-09-20 10:11:47 +0700

    perubahan pertama

---
+++ latihanminggu.py
@@ -0,0 +1,17 @@
+nama_peminjam = input()
+no_anggota = 1
+jumlah_buku = 5
+status = True
+durasi = 8
+
+print (nama_peminjam)
+
+jumlah_tiket="4"
+harga_tiket= 25000
+
+total_sementara= jumlah_tiket*2
+"Total sementara:",(total_sementara, "harga tiket:",harga_tiket)
+
+jumlah_tiket=int(jumlah_tiket)
+total_akhir= jumlah_tiket * harga_tiket
+print("total akhir:,total",total_akhir)
