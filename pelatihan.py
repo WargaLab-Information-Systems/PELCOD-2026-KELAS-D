84c4bb1342bad12634fa881fd439387bd1c9e6dd
Thoriq <thoriqaja3110@gmail.com>
2026-09-20 08:46:21 +0700

    EhhhBelajarDonghh

---
+++ pelatihan.py
@@ -0,0 +1,36 @@
+# DAY 1
+
+# nama1 ="Toyik"
+# nama2 = "naufal"
+# umur = 19
+# list = ["bobo", "mangan", "main"]
+
+# angka1 = 10
+# angka2 = 3
+
+# //
+# /
+# %
+
+# angka1 = 50
+# angka2 = 25
+# print(angka1 * angka2)
+
+# -
+# +
+# *
+
+# x = 10
+# print(x < 2 or x < 10)
+
+# nilai = 75
+
+# if nilai > 80:
+#     print("Lulus")
+# elif nilai > 50:
+#     print("REMEDIAL")
+# else:
+#     print ("TIDAK LULUS")
+
+# DAY 2
+
