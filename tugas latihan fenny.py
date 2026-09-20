b61dd6a880f00781991cf8ab201e34882761cf7a
unknown <fennyekasaputri04@gmail.com>
2026-09-20 10:12:02 +0700

    perubahan pertama

---
+++ tugas latihan fenny.py
@@ -0,0 +1,17 @@
+nama_peminjam = "Fenny"
+no_anggota = 1
+jumlah_buku = 4
+status = True 
+durasi = 8
+
+print(nama_peminjam)
+
+jumlah_tiket ="4"
+harga_tiket = 25000
+
+total_sementara = jumlah_tiket * 2
+print("Total sementara:", total_sementara) 
+
+jumlah_tiket = int(jumlah_tiket)
+total_akhir = jumlah_tiket*harga_tiket
+print("Total akhir.",total_akhir)
