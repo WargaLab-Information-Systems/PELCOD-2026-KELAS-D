ba301e5dad9ce3542e21211716a0fb1c28be5acc
Thoriq <thoriqaja3110@gmail.com>
2026-09-20 10:09:19 +0700

    Toyikk

--- pelatihan.py
+++ pelatihan.py
@@ -34,3 +34,38 @@
 
 # DAY 2
 
+# Nama_peserta = "toyik"
+# Nilai_tugas = 80
+# Nilai_kuis = 90
+# Nilai_uts = 87
+# kehadiran = 90
+
+# if kehadiran >= 75:
+#     if Nilai_tugas >= 80 and Nilai_kuis >= 80 and Nilai_uts >= 80:
+#         print("Selamat", Nama_peserta, "Anda Lulus")
+#     else:
+#         print("Maaf", Nama_peserta, "Anda Tidak Lulus")
+
+# nama_peminjam = "toyik"
+# nama_peminjam = input("Masukkan Nama Peminjam: ")
+# no_anggota = "123456"
+# jumqlah_buku = 3
+# status_anggota = "Aktif"
+# durasi_peminjaman = 5
+
+
+# print("Nama Peminjam:", nama_peminjam)
+# print("Nomor Anggota:", no_anggota)
+# print("Jumlah Buku yang Dipinjam:", jumqlah_buku)
+# print("Status Anggota:", status_anggota)
+# print("Durasi Peminjaman:", durasi_peminjaman, "hari")
+
+jumlah_tiket = "4"
+harga_tiket = 25000
+
+total_sementara = jumlah_tiket * 2
+print("Total sementara:", total_sementara) #"harga tiket:", harga_tiket)
+
+jumlah_tiket = int(jumlah_tiket)
+total_akhir = jumlah_tiket * harga_tiket
+print("Total akhir:", total_akhir)
